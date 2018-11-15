b95a996f0231d16d11acb7a84443abd96d25e4c5
archimedon <ronniedz@icloud.com>
2018-11-15 14:03:35 -0800

    Initial application generated by JHipster-5.6.0

--- src/main/webapp/app/shared/layout/header/menus/entities.tsx
+++ src/main/webapp/app/shared/layout/header/menus/entities.tsx
@@ -8,34 +8,6 @@ import { NavDropdown } from '../header-components';
 export const EntitiesMenu = props => (
   // tslint:disable-next-line:jsx-self-close
   <NavDropdown icon="th-list" name="Entities" id="entity-menu">
-    <DropdownItem tag={Link} to="/entity/question">
-      <FontAwesomeIcon icon="asterisk" />
-      &nbsp;Question
-    </DropdownItem>
-    <DropdownItem tag={Link} to="/entity/answer">
-      <FontAwesomeIcon icon="asterisk" />
-      &nbsp;Answer
-    </DropdownItem>
-    <DropdownItem tag={Link} to="/entity/track">
-      <FontAwesomeIcon icon="asterisk" />
-      &nbsp;Track
-    </DropdownItem>
-    <DropdownItem tag={Link} to="/entity/lesson">
-      <FontAwesomeIcon icon="asterisk" />
-      &nbsp;Lesson
-    </DropdownItem>
-    <DropdownItem tag={Link} to="/entity/course">
-      <FontAwesomeIcon icon="asterisk" />
-      &nbsp;Course
-    </DropdownItem>
-    <DropdownItem tag={Link} to="/entity/instruction">
-      <FontAwesomeIcon icon="asterisk" />
-      &nbsp;Instruction
-    </DropdownItem>
-    <DropdownItem tag={Link} to="/entity/file">
-      <FontAwesomeIcon icon="asterisk" />
-      &nbsp;File
-    </DropdownItem>
     {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
   </NavDropdown>
 );
--- src/main/webapp/app/shared/reducers/index.ts
+++ src/main/webapp/app/shared/reducers/index.ts
@@ -11,34 +11,6 @@ import activate, { ActivateState } from 'app/modules/account/activate/activate.r
 import password, { PasswordState } from 'app/modules/account/password/password.reducer';
 import settings, { SettingsState } from 'app/modules/account/settings/settings.reducer';
 import passwordReset, { PasswordResetState } from 'app/modules/account/password-reset/password-reset.reducer';
-// prettier-ignore
-import question, {
-  QuestionState
-} from 'app/entities/question/question.reducer';
-// prettier-ignore
-import answer, {
-  AnswerState
-} from 'app/entities/answer/answer.reducer';
-// prettier-ignore
-import track, {
-  TrackState
-} from 'app/entities/track/track.reducer';
-// prettier-ignore
-import lesson, {
-  LessonState
-} from 'app/entities/lesson/lesson.reducer';
-// prettier-ignore
-import course, {
-  CourseState
-} from 'app/entities/course/course.reducer';
-// prettier-ignore
-import instruction, {
-  InstructionState
-} from 'app/entities/instruction/instruction.reducer';
-// prettier-ignore
-import file, {
-  FileState
-} from 'app/entities/file/file.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 export interface IRootState {
@@ -51,13 +23,6 @@ export interface IRootState {
   readonly passwordReset: PasswordResetState;
   readonly password: PasswordState;
   readonly settings: SettingsState;
-  readonly question: QuestionState;
-  readonly answer: AnswerState;
-  readonly track: TrackState;
-  readonly lesson: LessonState;
-  readonly course: CourseState;
-  readonly instruction: InstructionState;
-  readonly file: FileState;
   /* jhipster-needle-add-reducer-type - JHipster will add reducer type here */
   readonly loadingBar: any;
 }
@@ -72,13 +37,6 @@ const rootReducer = combineReducers<IRootState>({
   passwordReset,
   password,
   settings,
-  question,
-  answer,
-  track,
-  lesson,
-  course,
-  instruction,
-  file,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
   loadingBar
 });
